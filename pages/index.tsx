5f8c829677c81876cd9c45435f6b8893584c855d
YILBERT MARQUEZ <Briyeltmarmarquez@gmail.com>
2024-09-21 15:05:21 -0500

    Finalizando segundo video: Estructura del prproyecto'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 
-import styles from "@/styles/Home.module.css";
+import { Typography } from '@mui/material';
+import { ShopLayout } from '../components/layouts';
 
 export default function Home() {
   return (
-    <>
-        <h1>Home Ecommerce</h1>
-    </>
-  );
+  <ShopLayout title={'Ecommerce Sena - Home'} pageDescription={'Encuentra tus mejores articulos para tu coleccion de Piedras'}>
+    <Typography variant='h1' component='h1'>Tienda</Typography>
+    <Typography variant='h2' sx={{mb: 1}}>Articulos</Typography>
+  </ShopLayout>
+  )
 }
